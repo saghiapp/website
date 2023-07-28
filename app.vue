e6ddcbb0178a9afc7b32146950e5b104c42a9aeb
Amirmasoud <amirmasood33@gmail.com>
2023-07-28 15:59:03 -0600

    one vote for simplicity

--- app.vue
+++ app.vue
@@ -50,53 +50,10 @@ getSurround(useRoute().path)
 </script>
 <template>
   <div dir="rtl" class="prose container mx-auto px-4 my-16" style="font-family: 'Noto Sans Arabic';">
-    <div class="flex justify-between items-center mb-16">
-      <div></div>
+    <div class="flex justify-center items-center mb-16">
       <NuxtLink to="/" class="text-center text-sm text-gray-500 block text-black">
         ساقی
       </NuxtLink>
-      <button type="button" @click.prevent="sidebarOpen = !sidebarOpen">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
-        </svg>
-        <TransitionRoot as="template" :show="sidebarOpen" class="text-right font-[]">
-          <Dialog as="div" class="relative z-50" @close="sidebarOpen = false">
-            <TransitionChild as="template" enter="transition-opacity ease-in-out duration-500" enter-from="opacity-0" enter-to="opacity-100" leave="transition-opacity ease-in-out duration-500" leave-from="opacity-100" leave-to="opacity-0">
-              <div class="fixed inset-0 bg-gray-400 bg-opacity-75" />
-            </TransitionChild>
-
-            <div class="fixed inset-0 z-40 flex">
-              <TransitionChild as="template" enter="transition ease-in-out duration-500 transform" enter-from="-translate-x-full" enter-to="translate-x-0" leave="transition ease-in-out duration-500 transform" leave-from="translate-x-0" leave-to="-translate-x-full">
-                <DialogPanel class="relative flex w-full max-w-xs flex-1 flex-col bg-gray-100 pt-5 pb-4">
-                  <TransitionChild as="template" enter="ease-in-out duration-500" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in-out duration-500" leave-from="opacity-100" leave-to="opacity-0">
-                    <div class="absolute top-0 right-0 -mr-12 pt-2">
-                      <button type="button" class="ml-1 flex h-10 w-10 items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" @click="sidebarOpen = false">
-                        <span class="sr-only">Close sidebar</span>
-                        <XMarkIcon class="h-6 w-6 text-white" aria-hidden="true" />
-                      </button>
-                    </div>
-                  </TransitionChild>
-                  <div class="h-0 flex-1 overflow-y-auto">
-                    <div class="space-y-1 px-4">
-                      <ContentNavigation 
-                        style="font-family: 'Noto Sans Arabic';"
-                        :query="queryContent({
-                          where: {
-                            _path: { $contains: $route.path }
-                          }
-                        })" 
-                      />
-                    </div>
-                  </div>
-                </DialogPanel>
-              </TransitionChild>
-              <div class="w-14 flex-shrink-0" aria-hidden="true">
-                <!-- Dummy element to force sidebar to shrink to fit close icon -->
-              </div>
-            </div>
-          </Dialog>
-        </TransitionRoot>
-      </button>
     </div>
     <NuxtPage />
     <div v-if="!ignoreSurround()" class="flex justify-center gap-x-8 mt-16">
